24bdf66e7b542914a0f7b14d11f6173eb14350fc
Nilesh Thakre <bluevelocity01@proton.me>
2025-02-09 00:25:04 +0530

    Delete raylib_projects/axe_game directory

--- raylib_projects/axe_game/build/build_files/obj/x64/Debug/axe_game0/main.d
+++
@@ -1,8 +0,0 @@
-obj/x64/Debug/axe_game0/main.o: ../../src/main.c \
- /usr/include/stdc-predef.h ../external/raylib-master/src/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/14/include/stdarg.h \
- /usr/lib/gcc/x86_64-redhat-linux/14/include/stdbool.h
-/usr/include/stdc-predef.h:
-../external/raylib-master/src/raylib.h:
-/usr/lib/gcc/x86_64-redhat-linux/14/include/stdarg.h:
-/usr/lib/gcc/x86_64-redhat-linux/14/include/stdbool.h:
